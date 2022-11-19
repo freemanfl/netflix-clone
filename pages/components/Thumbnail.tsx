19c57a64562af93892058c54b8aac0ad7ea6e69d
unknown <mm.farid@post.cz>
2022-11-19 23:05:58 +0100

    psl work

--- pages/components/Thumbnail.tsx
+++ pages/components/Thumbnail.tsx
@@ -5,14 +5,16 @@ interface Props {
     movie: Movie
 }
 const Thumbnail = ({movie}: Props) => {
+    
   return (
+    
     <div className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out 
                     md:h-36 md:min-w-[260px] md:hover:scale-105'>
         
         <Image 
             alt="thumbnail"
-            src={`https://image.tmdb.org/t/p/w500${ movie.backdrop_path ?
-                movie.backdrop_path : movie.poster_path
+            src={`https://image.tmdb.org/t/p/w500${ movie &&
+                movie.backdrop_path || movie.poster_path
             }`}
             className="rounded-sm object-cover md:rounded"
             fill={true}
